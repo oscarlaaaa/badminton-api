e03cef7974324a59b639e6d064fe23a09e9eca21
Oscar La <oscarla5747@gmail.com>
2021-11-05 02:06:38 -0700

    added name text cleaner; tournament name and year parsing

--- bwf-api/main.py
+++ bwf-api/main.py
@@ -1,4 +1,7 @@
 import requests
+import string
+import re
+
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
@@ -14,6 +17,11 @@ def convert_to_draws_link(tournament_id):
 ## takes in a draw link, and converts it into a link with all matches displayed for scraping
 def convert_to_matches_link(draw_link):
     return f"https://bwf.tournamentsoftware.com/sport/{draw_link[0:4]}matches{draw_link[4:]}"
+
+def clean_name_formatting(name):
+    name = re.sub('[^a-zA-Z]', ' ', name)
+    return name
+
 
 ## finds all draws that correspond to the event parameter, and returns an array of draw links
 def collect_draw_links(html_text, event):
@@ -32,6 +40,23 @@ def collect_draw_links(html_text, event):
 
     return desired_draws
 
+def collect_match_row_data(match_row, matches, tournament_title):
+    if match_row.find('span', class_='score') is None:
+        return
+    
+    players = [name.text for name in match_row.find_all('a') if "player" in name['href']]
+    # players = [name.text.encode('utf-8') for name in match_row.find_all('a') if "player" in name['href']]
+    if len(players) == 1:
+        return
+
+    winner = clean_name_formatting(players[0])
+    loser = clean_name_formatting(players[1])
+    points = [list(map(int, score.text.split('-'))) for score in match_row.find('span', class_='score').find_all('span')]
+    date_time = match_row.find('td', class_='plannedtime').text
+    year = date_time[-4:]
+
+    matches.append(Match(winner, loser, points, year, tournament_title))
+
 ## scrapes all match links, and returns a list of match objects
 def collect_all_matches(match_link):
     html_text = requests.get(match_link).text
@@ -40,24 +65,15 @@ def collect_all_matches(match_link):
     match_section = soup.find('table', class_='ruler matches').find('tbody', recursive=False)
     match_rows = match_section.find_all('tr', recursive=False)
 
+    tournament_title = soup.find('h2', class_='media__title media__title--large').text
+    print(tournament_title)
+
     matches = []
 
     for match_row in match_rows:
-        if match_row.find('span', class_='score') is None:
-            continue
-    
-        players = [name.text.encode('utf-8') for name in match_row.find_all('a') if "player" in name['href']]
-        if len(players) == 1:
-            continue
-
-        date_time = match_row.find('td', class_='plannedtime').text
-        points = [list(map(int, score.text.split('-'))) for score in match_row.find('span', class_='score').find_all('span')]
-        matches.append(Match(players[0], players[1], points))
+        collect_match_row_data(match_row, matches, tournament_title)
 
     return matches
-
-    
-
 
 
 ## outputs a full list of all match data scraped for that tournament and event for storage
