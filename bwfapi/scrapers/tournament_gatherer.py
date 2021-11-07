3d47ece1a5e969541f161be5681857d5e69f22ad
Oscar La <oscarla5747@gmail.com>
2021-11-07 01:34:54 -0700

    changed tournament links to ids

--- bwfapi/scrapers/tournament_gatherer.py
+++ bwfapi/scrapers/tournament_gatherer.py
@@ -13,7 +13,7 @@ class TournamentGatherer:
 
         self.end_year = end_year
 
-    def grab_tournament_from_year(year):
+    def grab_tournaments_from_year(self, year):
         link = f"https://bwf.tournamentsoftware.com/find?DateFilterType=0&StartDate={year}-01-01&EndDate={year}-12-31&page=5&TournamentCategoryIDList%5B0%5D=false&TournamentCategoryIDList%5B1%5D=false&TournamentCategoryIDList%5B2%5D=false&TournamentCategoryIDList%5B3%5D=false&TournamentCategoryIDList%5B4%5D=false&TournamentCategoryIDList%5B5%5D=20&TournamentCategoryIDList%5B6%5D=21&TournamentCategoryIDList%5B7%5D=22&TournamentCategoryIDList%5B8%5D=23&TournamentCategoryIDList%5B9%5D=24&TournamentCategoryIDList%5B10%5D=25&TournamentCategoryIDList%5B11%5D=1&TournamentCategoryIDList%5B12%5D=8&TournamentCategoryIDList%5B13%5D=2&TournamentCategoryIDList%5B14%5D=3&TournamentCategoryIDList%5B15%5D=4&TournamentCategoryIDList%5B16%5D=false&TournamentCategoryIDList%5B17%5D=false&TournamentCategoryIDList%5B18%5D=false&TournamentCategoryIDList%5B19%5D=false&TournamentCategoryIDList%5B20%5D=false&TournamentCategoryIDList%5B21%5D=false&TournamentCategoryIDList%5B22%5D=false&TournamentCategoryIDList%5B23%5D=false&TournamentCategoryIDList%5B24%5D=false&TournamentCategoryIDList%5B25%5D=false&TournamentCategoryIDList%5B26%5D=false&TournamentCategoryIDList%5B27%5D=false&TournamentCategoryIDList%5B28%5D=false&TournamentCategoryIDList%5B29%5D=false&TournamentCategoryIDList%5B30%5D=false&TournamentCategoryIDList%5B31%5D=false&TournamentCategoryIDList%5B32%5D=false"
 
         s = HTMLSession()
@@ -22,6 +22,7 @@ class TournamentGatherer:
 
         content = response.html.find('#searchResultArea', first=True).links
         links = list(content)
+        links = list(link[link.index('=') + 1:] for link in links)
 
         return {'year': year, 'links': links}
 
@@ -29,7 +30,7 @@ class TournamentGatherer:
     def grab_all_tournaments(self):
         links = []
         for i in range(2007, self.MAX_END_YEAR):
-            links.append(self.grab_tournament_from_year(i))
+            links.append(self.grab_tournaments_from_year(i))
         return links
 
     ## test function - should be 507
