11e825d7d3b1fc018e68db1b77ae1a6138e43c79
Oscar La <oscarla5747@gmail.com>
2021-11-08 02:32:34 -0800

    processes match data with no issues!

--- bwfapi/scrapers/main_scraper.py
+++ bwfapi/scrapers/main_scraper.py
@@ -1,29 +1,31 @@
 import csv
+from progress_bar import ProgressBar
 from tournament_gatherer import TournamentGatherer
 from match_gatherer import MatchGatherer
-from match import Match
-
+import match
 
 ####### START OF SCRIPT ########
 
+# Gathers tournment links from certain year
 tg = TournamentGatherer()
-
 tournaments = tg.grab_tournaments_from_year(2010)
-print(str(tournaments['links']))
 
-mg = MatchGatherer("MS", year=tournaments['year'])
-for t in tournaments['links']:
-    mg.collect_match_data(t)
-
+# Gathers matches from a set year of tournaments
+mg = MatchGatherer(year=tournaments['year'], event="MS", tournament_list=tournaments['links'])
+mg.collect_all_match_data()
+    
+# Match list to be mass-processed into csv
 match_list = mg.get_match_list()
+csv_bar = ProgressBar(len(match_list), prefix = 'Writing into .csv', suffix = 'of data written.')
 
-for match in match_list:
-    print(str(match))
+with open('test.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
 
+    count = 1
+    total = len(match_list)
 
-# with open('test.csv', 'w', newline='') as file:
-#     writer = csv.writer(file)
-#     writer.writerow(["SN", "Name", "Contribution"])
-#     writer.writerow([1, "Linus Torvalds", "Linux Kernel"])
-#     writer.writerow([2, "Tim Berners-Lee", "World Wide Web"])
-#     writer.writerow([3, "Guido van Rossum", "Python Programmingxdd"])
+    writer.writerow(match.header_row())
+    for match in match_list:
+        writer.writerow([str(count), match.get_tournament(), match.get_date(), match.get_time(), match.get_winner(), match.get_loser(), match.get_points(), match.get_duration()])
+        count = count + 1
+        csv_bar.printProgressBar()
