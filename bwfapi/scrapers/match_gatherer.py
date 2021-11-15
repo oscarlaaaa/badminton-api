0eeb6060d1f726e839eb9a17877191467f82c85c
Oscar La <oscarla5747@gmail.com>
2021-11-15 04:32:14 -0800

    added returns to replace self-mutation; tried async but a bit slow

--- bwfapi/scrapers/match_gatherer.py
+++ bwfapi/scrapers/match_gatherer.py
@@ -1,21 +1,24 @@
 import requests
+
 from datetime import datetime
 from bs4 import BeautifulSoup
 
 from .progress_bar import ProgressBar
 from .match import Match
 
+# import nest_asyncio
+# nest_asyncio.apply()
+
 class MatchGatherer:
 
     PLAYERS_IN_ROW = 2
 
-    def __init__(self, year=0, event="MS", tournament_list=[]):
+    def __init__(self, event="MS", tournament_list=[]):
         event = event.upper().strip()
 
         if event != "MS" and event != "WS":
             raise NameError("Not a valid event. Please input either MS or WS")
 
-        self.year = year
         self.event = event
 
         self.tournament_list = tournament_list
@@ -96,7 +99,7 @@ class MatchGatherer:
             return True
 
     ## collects and creates a match object from a given match row and appends to master matches list
-    def collect_match_row_data(self, match_row, matches, tournament_title, level):
+    def collect_match_row_data(self, match_row, tournament_title, level):
         if self.is_invalid_matchrow(match_row):
             return
         
@@ -115,11 +118,13 @@ class MatchGatherer:
             
         duration = self.convert_time_string_to_minutes([duration.text for duration in match_row.find_all('td')][-2])
 
-        matches.append(Match(self.event, winner, loser, points, date, time, duration, tournament_title, level))
+        return Match(self.event, winner, loser, points, date, time, duration, tournament_title, level)
 
     ## scrapes all match links, and returns a list of match objects
     def collect_all_matches(self, match_link, level):
+
         html_text = requests.get(match_link).text
+
         soup = BeautifulSoup(html_text, 'lxml')
 
         match_section = soup.find('table', class_='ruler matches').find('tbody', recursive=False)
@@ -127,17 +132,17 @@ class MatchGatherer:
 
         tournament_title = soup.find('h2', class_='media__title media__title--large').text
 
-        matches = []
-
-        for match_row in match_rows:
-            self.collect_match_row_data(match_row, matches, tournament_title, level)
-
+        matches = [match for match_row in match_rows if (match := self.collect_match_row_data(match_row, tournament_title, level)) is not None]
         return matches
 
 
     ## compiles a full list of all match data scraped for that tournament and event for storage
     def collect_match_data(self, tournament_id):
+
+        print(f"collecting data for {tournament_id}")
+
         draws_link = self.convert_to_draws_link(tournament_id)
+
         html_text = requests.get(draws_link).text
         
         relevant_draws = self.collect_draw_links(html_text, self.event)
@@ -146,26 +151,33 @@ class MatchGatherer:
         soup = BeautifulSoup(html_text, 'lxml')
 
         tournament_level = soup.find('span', class_='tag tag--mono').text
-
+        matches = []
         for match_link in relevant_match_links:
-            matches = self.collect_all_matches(match_link, tournament_level)
-            if matches != None:
-                self.match_list = self.match_list + matches
+            tournament_matches = self.collect_all_matches(match_link, tournament_level)
+            if tournament_matches is not None:
+                matches = matches + tournament_matches
+        return matches
 
     def sort_match_data(self):
+        for match in self.match_list:
+            print(str(match))
+
         self.match_list.sort(key=lambda match: datetime.strptime(match.get_date(), '%m/%d/%Y'))
 
     ## compiles a list of every match stored within it
     def collect_all_match_data(self, sort=True):
         tournament_bar = ProgressBar(total=len(self.tournament_list), prefix = 'Parsing tournament matches', suffix = 'of matches completed')
         for link in self.tournament_list:
-            self.collect_match_data(link)
+            self.match_list = self.match_list + self.collect_match_data(link)
             tournament_bar.printProgressBar()
         if sort:
             self.sort_match_data()
 
 
-    
+# def __main__():
+#     xd = MatchGatherer(tournament_list=["a6128cae-03b8-492c-a398-ad3505e8ec16"])
+#     zz = asyncio.run(xd.collect_all_match_data())
+#     print(len(zz))
+   
 ## tests for the fn
-# xd = collect_match_data("a6128cae-03b8-492c-a398-ad3505e8ec16", "MS")
-# print(len(xd))
+
