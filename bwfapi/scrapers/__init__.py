e513c6f561c88620ac5db37615bfca4cd2aae45c
Oscar La <oscarla5747@gmail.com>
2021-11-17 02:24:21 -0800

    add async match scrape import

--- bwfapi/scrapers/__init__.py
+++ bwfapi/scrapers/__init__.py
@@ -1,3 +1,4 @@
+from .match_gatherer_async import AsyncMatchGatherer
 from .match_gatherer import MatchGatherer
 from .progress_bar import ProgressBar
 from .tournament_gatherer import TournamentGatherer
