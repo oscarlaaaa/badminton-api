c6f6b94c42169986708788d970ce5d9dcf0d2dfa
Oscar La <oscarla5747@gmail.com>
2021-11-24 02:35:19 -0800

    integrated local MySQL access

--- bwfapi/server.py
+++ bwfapi/server.py
@@ -1,8 +1,11 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from web_scraper import set_db_login_credentials, initialize_db, BwfScraper
+
 
 app = Flask(__name__)
 api = Api(app)
+mysql = set_db_login_credentials(app)
 
 class HelloWorld(Resource):
     def get(self):
@@ -15,8 +18,21 @@ class Multi(Resource):
     def get(self, num):
         return {"result": num*10}
 
+class InitializeDB(Resource):
+    def get(self):
+        initialize_db(mysql)
+        return {"about": "Hello World!"}
+
+class Scrape(Resource):
+    def get(self, year, event):
+        scraper = BwfScraper(year, event)
+        t_time = scraper.scrape_tournaments()
+        m_time = scraper.scrape_matches()
+
 api.add_resource(HelloWorld, '/')
 api.add_resource(Multi, '/multi/<int:num>')
+api.add_resource(InitializeDB, '/init')
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
