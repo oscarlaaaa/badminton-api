77c28fbde4f3ddafca0eb9a0ff58daf35461539f
Oscar La <oscarla5747@gmail.com>
2021-11-26 04:29:36 -0800

    modularize player gathering

--- bwfapi/web_scraper/scrapers/__init__.py
+++ bwfapi/web_scraper/scrapers/__init__.py
@@ -3,3 +3,4 @@ from .match_gatherer import MatchGatherer
 from .progress_bar import ProgressBar
 from .tournament_gatherer import TournamentGatherer
 from .match import Match
+from .player_gatherer import PlayerGatherer
