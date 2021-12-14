e7bbbc5252e2215a5791e0f8fd0804efb582635d
Oscar La <67846380+oscarlaaaa@users.noreply.github.com>
2021-12-14 08:54:26 -0800

    add player info scraper for extra player stats

--- bwfapi/web_scraper/scrapers/player_gatherer.py
+++ bwfapi/web_scraper/scrapers/player_gatherer.py
@@ -11,8 +11,15 @@ class PlayerGatherer:
     def format_link(self, link):
         return f'https://bwf.tournamentsoftware.com/sport/{link}'
 
+    def format_player_link(self, link):
+        return f"https://bwf.tournamentsoftware.com/player-profile/{link}"
+
     def get_player_list(self):
         return self.player_list
+
+    def format_date(self, date):
+        vals = date.split('/')
+        return f"{vals[2]}-{vals[0]}-{vals[1]}"
 
     async def grab_player(self, link, session):
         # print(f"collecting data for player {link}")
@@ -24,15 +31,64 @@ class PlayerGatherer:
         soup = BeautifulSoup(html_text, 'lxml')
         player_name = soup.find('div', class_='subtitle').find('h2').text
 
-        link = soup.find('div', class_='subtitle').find('a')
+        info_link = soup.find('div', class_='subtitle').find('a')['href']
+
+        details = dict()
+        details['country'] = None
+        details['date of birth'] = None
+
+        name = player_name[:player_name.index("Profile")].upper().strip()
+        player_id = info_link[1:]
         
-        if link:
-            name = player_name[:player_name.index("Profile")].upper().strip()
-            player_id = link['href'][len('/player-profile/'):]
-            self.player_list[name] = player_id
+        index = player_id.index('/')+1
+        player_id = player_id[index:]
+
+        details['id'] = player_id
+        self.player_list[name] = details
+    
+    async def grab_player_info(self, details, session):
+        print("AAAA")
+        async with session.get(self.format_player_link(details['id'])) as resp:
+            html_text = await resp.text()
+        print("BBBB")
+        soup = BeautifulSoup(html_text, 'lxml')
+        detail_section = soup.find('dl', class_='list list--flex list--bordered').findAll('div', class_='list__item')
+        
+        country = soup.find('div', class_='media__content-subinfo')
+        if country:
+            country.find('span', class_='nav-link__value').text
+            country = country.upper().strip()
+            details['country'] = country
+        
+        for detail in detail_section:
+            section = detail.find('dt', class_='list__label').text.lower().strip()
+            value = detail.find('span', class_='nav-link__value').text.strip()
+
+            if section == 'date of birth':
+                value = self.format_date(value)
+
+            details[section] = value
+
+        # if link:
+        #     name = player_name[:player_name.index("Profile")].upper().strip()
+        #     player_id = link['href'][1:]
+        #     index = player_id.index('/')+1
+        #     player_id = player_id[index:]
+        #     self.player_list[name] = {"id": player_id}
     
     async def grab_all_players(self):
         async with aiohttp.ClientSession() as session:
             loop = asyncio.get_event_loop()
-            tasks = asyncio.gather(*[self.grab_player(player_link, session) for player_link in self.player_links], return_exceptions=True)
+            tasks = asyncio.gather(*[self.grab_player(link=player_link, session=session) for player_link in self.player_links], return_exceptions=True)
             loop.run_until_complete(tasks)
+
+    async def grab_all_player_info(self):
+        async with aiohttp.ClientSession() as session:
+            loop = asyncio.get_event_loop()
+            tasks = asyncio.gather(*[self.grab_player_info(details=details, session=session) for details in self.player_list.values()], return_exceptions=True)
+            loop.run_until_complete(tasks)
+
+# if __name__ == "__main__":
+#     pg = PlayerGatherer(['player.aspx?id=94646034-3486-42AD-8D5B-5CE3FB08D3D2&player=40'])
+#     loop = asyncio.get_event_loop()
+#     loop.run_until_complete(pg.grab_player('player.aspx?id=94646034-3486-42AD-8D5B-5CE3FB08D3D2&player=40',aiohttp.ClientSession()))
