989ac8fe3f8508d730fab564dd62a568b06228f7
Oscar La <67846380+oscarlaaaa@users.noreply.github.com>
2021-12-16 23:36:27 -0800

    conversion from flask to fastAPI

--- bwfapi/server.py
+++ bwfapi/server.py
@@ -1,34 +1,74 @@
-from flask import Flask, request, render_template
-from flask_restful import Resource, Api
-# from web_scraper import set_db_login_credentials, initialize_db, BwfScraper
+from fastapi import FastAPI, BackgroundTasks
+from fastapi_utils.tasks import repeat_every
+import time
+import asyncio
+import nest_asyncio
+from enum import Enum
+from web_scraper import scrape_current_month_matches, lol
+from web_scraper.db import DBOperator
+from datetime import date
 
-app = Flask(__name__, template_folder="templates")
-api = Api(app)
-# mysql = set_db_login_credentials(app)
+import logging
+from fastapi import FastAPI
+from web_scraper.services import EchoService
 
-class HelloWorld(Resource):
-    def get(self):
-        return app.send_static_file('index.html')
-    
-    def post(self):
-        return {"you sent" : request.get_json()}, 201
+nest_asyncio.apply()
 
-class Multi(Resource):
-    def get(self, num):
-        return {"result": num*10}
+# setup loggers
+logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
 
-# class InitializeDB(Resource):
-#     def get(self):
-#         try:
-#             initialize_db(mysql)
-#             return {"database": "success"}
-#         except:
-#             return {"database": "ERROR!"}
+# get root logger
+logger = logging.getLogger(__name__)  # the __name__ resolve to "main" since we are at the root of the project. 
+                                      # This will get the root logger since no logger in the configuration has this name.
 
-api.add_resource(HelloWorld, '/')
-api.add_resource(Multi, '/multi/<int:num>')
-# api.add_resource(InitializeDB, '/init')
+app = FastAPI()
 
-if __name__ == "__main__":
-    app.run(debug=True)
-    
+@app.on_event("startup")
+@repeat_every(seconds=60, raise_exceptions=True)  # 1 week
+async def update_database():
+    logger.info("Updating database")
+    EchoService.echo("Running scraping script")
+    await scrape_current_month_matches()
+    EchoService.echo("a scraping script")
+
+@app.get("/")
+async def root():
+    logger.info("logging from the root logger")
+    EchoService.echo("hi")
+    return {"message": "Hello World"}
+
+@app.get("/clear")
+async def reset_db():
+    logger.info("Clearing database")
+    EchoService.echo("Running clearing script")
+    db = DBOperator()
+    db.reset_database()
+    return {"message" : "Database cleared!"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
