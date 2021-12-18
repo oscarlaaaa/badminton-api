3da09570795096d7e56d92fe7cc6bb9b923cab45
Oscar La <67846380+oscarlaaaa@users.noreply.github.com>
2021-12-18 02:28:16 -0800

    add name search, id, match endpoints

--- bwfapi/api/crud.py
+++ bwfapi/api/crud.py
@@ -1,7 +1,25 @@
 from sqlalchemy.orm import Session
-
 from . import models
+from typing import Optional, Union
 
-def get_player(db: Session, player_name: str):
-    player_name = player_name.replace("_", " ").upper()
-    return db.query(models.Player).filter(models.Player.name == player_name).first()
+def format_response(data: Optional[Union[str, int]], req: str) -> Optional[dict]:
+    if data:
+        return {
+            "request" : req,
+            "status" : "success",
+            "status_code" : 200,
+            "data" : data
+        }
+
+def get_player(db: Session, player_id: str) -> Optional[dict]:
+    result = db.query(models.Player).filter(models.Player.id == player_id).first()
+    return format_response(result, f"GET request for single player {player_id}")
+
+def search_player(db: Session, search_text: str) -> Optional[dict]:
+    search_text = search_text.replace("_", " ").upper()
+    result = db.query(models.Player).filter(models.Player.name.contains(search_text)).limit(20).all()
+    return format_response(result, f"GET request players with {search_text} in their name")
+
+def get_player_matches(db: Session, player_id: str) -> Optional[dict]:
+    result = db.query(models.Match).filter((models.Match.winnerId == player_id) | (models.Match.loserId == player_id)).all()
+    return format_response(result, f"GET request matches from player id {player_id}")
--- bwfapi/server.py
+++ bwfapi/server.py
@@ -1,9 +1,6 @@
-import time
-import asyncio
-import nest_asyncio
-
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi_utils.tasks import repeat_every
+from fastapi.staticfiles import StaticFiles
 from sqlalchemy.orm import Session
 from enum import Enum
 from web_scraper import scrape_current_month_matches
@@ -16,13 +13,12 @@ from web_scraper.services import EchoService
 from api import crud, models
 from api import SessionLocal, engine
 
-nest_asyncio.apply()
-
 ## Snippet taken from: https://philstories.medium.com/fastapi-logging-f6237b84ea64
 logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 # Dependency
 def get_db():
@@ -32,46 +28,52 @@ def get_db():
     finally:
         db.close()
 
-@app.get("/players/{name}")
-async def get_player(name: str, db: Session=Depends(get_db)):
-    player = crud.get_player(db, player_name=name)
-    if player is None:
-        raise HTTPException(status_code=404, detail="Player could not found")
-    return player
-
 @app.on_event("startup")
 @repeat_every(seconds=999999, raise_exceptions=True, wait_first=True)  # 1 week
-async def update_database():
+async def update_database() -> dict:
     logger.info("Updating database")
     EchoService.echo("Running scraping script")
     await scrape_current_month_matches()
-    EchoService.echo("a scraping script")
+    EchoService.echo("Scraping script complete")
+    return {
+        "request" : "update database",
+        "status" : "success",
+    }
 
 @app.get("/")
-async def root():
+async def root() -> dict:
     logger.info("logging from the root logger")
     EchoService.echo("hi")
     return {"message": "Hello World"}
 
 @app.get("/clear")
-async def reset_db():
+async def reset_db() -> dict:
     logger.info("Clearing database")
     EchoService.echo("Running clearing script")
     db = DBOperator()
     db.reset_database()
-    return {"message" : "Database cleared!"}
+    return {
+        "request" : "clear database",
+        "status" : "success",
+    }
 
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
+@app.get("/players/{player_id}")
+async def get_player(player_id: str, db: Session=Depends(get_db)) -> dict:
+    player = crud.get_player(db, player_id=player_id)
+    if player is None:
+        raise HTTPException(status_code=404, detail="Player could not found")
+    return player
 
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name == ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+@app.get("/search") ## takes in /search?name=<STUFF>
+async def search_player(name: str, db: Session=Depends(get_db)) -> dict:
+    players = crud.search_player(db, search_text=name)
+    if players is None:
+        raise HTTPException(status_code=404, detail=f"No player with {name} in their name.")
+    return players
 
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-
-    return {"model_name": model_name, "message": "Have some residuals"}
+@app.get("/matches/{player_id}")
+async def search_player_matches(player_id: str, db: Session=Depends(get_db)) -> dict:
+    matches = crud.get_player_matches(db, player_id=player_id)
+    if matches is None:
+        raise HTTPException(status_code=404, detail=f"No matches from {player_id}.")
+    return matches
