f530fe5fba732c378b59f329e1f5ff7c8fc12db8
Oscar La <67846380+oscarlaaaa@users.noreply.github.com>
2021-12-22 05:02:08 -0800

    add head-to-head, top players

---
+++ bwfapi/api/routes/player.py
@@ -0,0 +1,55 @@
+from fastapi import Depends, APIRouter
+from sqlalchemy.orm import Session
+
+from web_scraper.services import EchoService
+from api.exceptions import InvalidParameterException, NoResultsException
+from api import crud, dependencies
+
+router = APIRouter(
+    prefix="/player",
+    responses={404: {"description": "Not found"}}
+)
+
+@router.get("/")
+async def get_player(player_id: str="", db: Session=Depends(dependencies.get_db)) -> dict:
+    EchoService.echo(f"get_player called for player_id={player_id}")
+    player = crud.get_player(db, player_id=player_id)
+    if player is None:
+        raise NoResultsException(status_code=404, detail="Player could not found")
+    return player
+
+VALID_EVENTS = {"MS", "WS", ""}
+@router.get("/top")
+async def get_top_wins(event: str="", limit: int=10, db: Session=Depends(dependencies.get_db)) -> dict:
+    EchoService.echo(f"get_top_wins called for event={event}, limit={limit}")
+    if limit < 1:
+        raise InvalidParameterException(status_code=404, detail=f"Cannot have a search limit below 1.")
+    if event.upper() not in VALID_EVENTS:
+        raise InvalidParameterException(status_code=404, detail=f"Event must be WS, MS, or blank.")
+
+    players = crud.get_top_win_players(db, event=event, limit=limit)
+    if players is None:
+        raise NoResultsException(status_code=404, detail="Error in querying top players")
+    return players
+
+@router.get("/search") ## takes in /search?name=<STUFF>
+async def search_player(name: str="", limit: int=20, db: Session=Depends(dependencies.get_db)) -> dict:
+    EchoService.echo(f"search_player called for name={name}, limit={limit}")
+    if limit < 1:
+        raise InvalidParameterException(status_code=404, detail=f"Cannot have a search limit below 1.")
+
+    players = crud.search_player(db, search_text=name, limit=limit)
+    if players is None:
+        raise NoResultsException(status_code=404, detail=f"No player with {name} in their name.")
+    return players
+
+@router.get("/h2h")
+async def get_head_to_heads(player_id: str="", limit: int=10, db: Session=Depends(dependencies.get_db)) -> dict:
+    EchoService.echo(f"get_head_to_heads called for player_id={player_id}, limit={limit}")
+    if limit < 1:
+        raise InvalidParameterException(status_code=404, detail=f"Cannot have a search limit below 1.")
+
+    h2h = crud.get_player_head_to_heads(db, player_id=player_id, limit=limit)
+    if h2h is None:
+        raise NoResultsException(status_code=404, detail=f"No head to heads found for {player_id}.")
+    return h2h
