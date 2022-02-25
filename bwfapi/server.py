128952e8a083caa5dc05e0fc4bf142529d69ce7b
Oscar <oscarla5747@gmail.com>
2022-02-24 17:05:48 -0800

    Fix CORS

--- bwfapi/server.py
+++ bwfapi/server.py
@@ -22,6 +22,7 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi_utils.tasks import repeat_every
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import RedirectResponse
 import logging
 from os import path
@@ -36,6 +37,19 @@ logger = logging.getLogger(__name__)
 
 app = FastAPI()
 app.mount("/index", StaticFiles(directory="bwfapi/static", html=True), name="static")
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 wait = random.randint(0, 5)
 @app.on_event("startup")
